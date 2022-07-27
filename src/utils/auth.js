c7583a83d4797d9a19aa8920dcdf5e9a49e4de4c
Leonid <dolgodvorov.l@yandex.ru>
2022-07-27 22:38:51 +0500

    Добавлены запросы по авторизации в auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,37 @@
+const BASE_URL = "https://auth.nomoreparties.co";
+
+returnResJson = (res) =>{
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка: ${res.status}`);
+}
+
+export const register = (password, email) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      password: password,
+      email: email,
+    }),
+  })
+    .then(returnResJson)
+};
+
+export const authorize = (password, email) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      password: password,
+      email: email,
+    }),
+  })
+    .then(returnResJson)
+};
+
